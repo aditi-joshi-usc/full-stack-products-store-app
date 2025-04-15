e15605a9eaa295b3709d81a9b767dc743daf30d4
aditi-joshi-usc <aajoshi@usc.edu>
2025-04-15 15:58:16 -0700

    update port

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000; // Use the port from environment variables or default to 5000
 app.use(express.json()); // Middleware to parse JSON data from the request body
 
 app.use("/api/products", productRoutes); // Mount the product routes on the /api/products path
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     
     connectDB();
     console.log("Server is running on port 5000 ");
